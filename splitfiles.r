9c50de71f678fb20e4c3200b135c09a9cbb350bc
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 17:06:11 -0700

    adding initial files

--- splitfiles.r
+++ splitfiles.r
@@ -1,63 +1,47 @@
-library(readr)
-library(magrittr)
-vol <- scan("FULL_TXT/Vol1-ToSplit.txt", sep="\n", what="character")
-meta <- read_csv("TOCMetaData.csv")
+require(readr)
+require(magrittr)
 
-grep("\f\\(13)\\)", ignore.case=TRUE, vol1)
 
-metarows <- nrow(meta)
-for (x in 1:metarows) {
-  if (x != metarows){
-    #cat(paste("printing row #",x, sep=""))
-    page1 <- print(paste("#\\(",meta$Page[x],"\\)", sep=""))
-    page2 <- print(paste("#\\(",meta$Page[x + 1],"\\)", sep=""))
-    pageno <- grep(page1, ignore.case=TRUE, vol)
-    pageno2 <- grep(page2, ignore.case=TRUE, vol)
-    lines <- print(vol[pageno:pageno2])
-    filename <- print(paste("ETO",meta$ETO[x],meta$accused,".txt", sep=""))
-    fileConn<-file(paste("IndivCases/", filename))
-    writeLines(lines, fileConn)
-    close(fileConn)
-  } else {
-    page1 <- print(paste("#\\(",meta$Page[metarows],"\\)", sep=""))
-    enddoc <- length(vol)
-    pageno <- grep(page1, ignore.case=TRUE, vol)
-    lines <- print(vol[pageno:enddoc])
-    filename <- print(paste("ETO",meta$ETO[metarows],meta$accused,".txt", sep=""))
-    fileConn<-file(paste("IndivCases/", filename))
-    writeLines(lines, fileConn)
-    close(fileConn)
+
+
+
+
+create.individual.docs <- function (filepath, volumeno) {
+  meta <- read_csv("TOCMetaData.csv")
+  volumenumber <- paste("vol", volumeno, sep="")
+  metasub <- filter(meta, meta$volume == volumenumber)
+  vol <- scan(filepath, sep="\n", what="character")
+  metarows <- nrow(metasub)
+  for (x in 1:metarows) {
+    if (x != metarows){
+      #cat(paste("printing row #",x, sep=""))
+      page1 <- print(paste("#\\(",metasub$Page[x],"\\)", sep=""))
+      page2 <- print(paste("#\\(",metasub$Page[x + 1],"\\)", sep=""))
+      pageno <- grep(page1, ignore.case=TRUE, vol)
+      pageno2 <- grep(page2, ignore.case=TRUE, vol)
+      lines <- print(vol[pageno:pageno2])
+      filename <- print(paste("ETO",metasub$ETO[x],metasub$accused,".txt", sep=""))
+      fileConn<-file(paste("IndivCases/", filename))
+      writeLines(lines, fileConn)
+      close(fileConn)
+    } else {
+      page1 <- print(paste("#\\(",metasub$Page[metarows],"\\)", sep=""))
+      enddoc <- length(vol)
+      pageno <- grep(page1, ignore.case=TRUE, vol)
+      lines <- print(vol[pageno:enddoc])
+      filename <- print(paste("ETO",metasub$ETO[metarows],metasub$accused,".txt", sep=""))
+      fileConn<-file(paste("IndivCases/", filename))
+      writeLines(lines, fileConn)
+      close(fileConn)
+    }
   }
 }
 
 
-make.files <- function(files, input.dir) {
-  for (x in 1:length(files)) {
-    text <- scan(files[x], sep="\n", what="character")
-    text.lower <- tolower(text)
-    volpos.v <- grep("(^Vol.) [\\XVlILNU]", ignore.case = TRUE, text)
-    for (i in 1:length(volpos.v)){
-      if (i != length(volpos.v)){
-        start <- volpos.v[i]
-        end <- volpos.v[i+1]
-        volume.lines.l <- text[start:end]
-        filename <- print(paste(text[start],".txt"))
-        fileConn<-file(paste("IndivVolumes/", filename))
-        writeLines(volume.lines.l, fileConn)
-        close(fileConn)
-      } else {
-        start <- tail(volpos.v, n=1)
-        end <- length(text)
-        vol.title <- text[[start]]
-        lastlines <- text[start:end]
-        filename <- print(paste(text[start],".txt"))
-        fileConn<-file(paste("IndivVolumes/", filename))
-        writeLines(volume.lines.l, fileConn)
-        close(fileConn)
-      }
-    } 
-  }
-}
+
 #check
-nrow(meta)
-length(list.files("IndivCases/"))
+checkfiles <- function() {
+  print(nrow(meta))
+  print(length(list.files("IndivCases/")))
+}
+  
